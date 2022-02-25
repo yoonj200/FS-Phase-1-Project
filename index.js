d2156eba305e1ff25c189d8d522e4c62b1ae0c48
Joshua <yoonj200@gmail.com>
2022-02-25 14:49:58 -0600

    JS declare variables globally

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 window.onload = function () { // execute function after page loads
-
+    // Declaring variables globally
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var topics;         // Array of topics
+    var chosenTopic;    // Selected topic
+    var getHint;        // Word getHint
+    var word;           // Selected word
+    var guess;          // Guess
+    var guesses = [];   // Stored guesses
+    var lives;          // Lives
+    var counter;        // Count correct guesses
+    var space;          // Number of spaces in word '-'
+   
+    
+   
   } // end function
   
