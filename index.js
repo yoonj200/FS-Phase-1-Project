d99a36c1c53c4a4a3acb45d3cb3ed4fc34266fd7
Joshua <yoonj200@gmail.com>
2022-03-01 17:14:30 -0600

    More CSS alterations + changes one word and its hint

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ window.onload = function () {
     play = function () {
       topics = [
         ["cleopatra", "napoleon-bonaparte", "julius-caesar", "marcus-aurelius", "abraham-lincoln", "grace-kelly", "akihito"],
-        ["alien", "dirty-harry", "gone-with-the-wind", "finding-nemo", "forrest-gump"],
+        ["alien", "dirty-harry", "the-matrix", "finding-nemo", "forrest-gump"],
         ["taipei", "milan", "madrid", "amsterdam", "prague"] 
       ];
    
@@ -129,7 +129,7 @@ window.onload = function () {
         ["Last true pharaoh of Egypt", "French Emperor from 1804 to 1814", "Led Rome\'s armies in the Gallic Wars", 
           "A stoic philosopher and Roman emperor", "16th President of the United States of America", 
           "An early 20th-century American actress who became the Princess of Monaco", "Japan's first emperor to abdicate the throne since 1817"],
-        ["1979 Sci-fi horror film", "\"Go ahead. Make my day.\"", "\"Frankly, my dear, I don't give a damn.\"", "\"Fish are friends, not food.\"", 
+        ["1979 Sci-fi horror film", "\"Go ahead. Make my day.\"", "\"Ignorance is bliss.\"", "\"Fish are friends, not food.\"", 
          "\"I'm pretty tired... I think I'll go home now.\""],
         ["Known for its Shilin market", "The global capital of fashion and design", "Home of the Prado Museum", "Home of the Van Gogh Museum", 
          "Known for its medieval Astronomical Clock "]
