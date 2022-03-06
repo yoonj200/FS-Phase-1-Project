a4dab1d07b8d7f95e1924b9aed9d624284760207
Joshua <yoonj200@gmail.com>
2022-03-05 22:23:08 -0600

    Create topic selector function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-// Create alphabet ul ------------------------------
+// Create alphabet ul --------------------------------------------------
 let buttons = function () {
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                       'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -19,6 +19,33 @@ let buttons = function () {
   }
 }
 buttons();
+
+// Topic selector --------------------------------------------------
+let selectTopic = function () {
+  const topics = [
+    ["cleopatra", "napoleon-bonaparte", "julius-caesar", "marcus-aurelius", "abraham-lincoln", "grace-kelly", "akihito"],
+    ["alien", "dirty-harry", "the-matrix", "finding-nemo", "forrest-gump"],
+    ["taipei", "milan", "madrid", "amsterdam", "prague"] 
+  ];
+  
+  let chosenTopic = topics[Math.floor(Math.random() * topics.length)];
+  let word = chosenTopic[Math.floor(Math.random() * chosenTopic.length)];
+  // let word = word.replace(/\s/g, "-");
+  // console.log(word);
+
+  if (chosenTopic === topics[0]) {
+    topicName.innerHTML = "WORD TOPIC: Historical Figures";
+    console.log("WORD TOPIC: Historical Figures")
+  } else if (chosenTopic === topics[1]) {
+    topicName.innerHTML = "WORD TOPIC: Films";
+    console.log("WORD TOPIC: Films")
+  } else if (chosenTopic === topics[2]) {
+    topicName.innerHTML = "WORD TOPIC: Cities";
+    console.log("WORD TOPIC: Cities")
+  }
+}
+selectTopic();
+
 
 })
   
