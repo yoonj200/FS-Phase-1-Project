77bde45502c68fcd41ae207749e40caa577c88da
Joshua <yoonj200@gmail.com>
2022-03-14 13:03:53 -0500

    Creates nested arrays for topics in respective categories

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM fully loaded and parsed')
-  
+  // console.log('DOM fully loaded and parsed')
+let topics = [
+  ["cleopatra", "napoleon-bonaparte", "julius-caesar", "marcus-aurelius", "abraham-lincoln", "grace-kelly", "akihito"],
+  ["alien", "dirty-harry", "the-matrix", "finding-nemo", "forrest-gump"],
+  ["taipei", "milan", "madrid", "amsterdam", "prague"] 
+];
+
+
 })
