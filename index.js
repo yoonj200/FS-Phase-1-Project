392797672f72597c4a8960681a884623cc651472
Joshua <yoonj200@gmail.com>
2022-03-15 21:09:57 -0500

    Randomly selects word

--- index.js
+++ index.js
@@ -21,21 +21,29 @@ function topicSelector() {
     ["alien", "dirty-harry", "the-matrix", "finding-nemo", "forrest-gump"],
     ["taipei", "milan", "madrid", "amsterdam", "prague"] 
   ];
-
+  hints = [
+    ["Last true pharaoh of Egypt", "French Emperor from 1804 to 1814", "Led Rome\'s armies in the Gallic Wars", 
+      "A stoic philosopher and Roman emperor", "16th President of the United States of America", 
+      "An early 20th-century American actress who became the Princess of Monaco", "Japan's first emperor to abdicate the throne since 1817"],
+    ["1979 Sci-fi horror film", "\"Go ahead. Make my day.\"", "\"Ignorance is bliss.\"", "\"Fish are friends, not food.\"", 
+      "\"I'm pretty tired... I think I'll go home now.\""],
+    ["Known for its Shilin market", "The global capital of fashion and design", "Home of the Prado Museum", "Home of the Van Gogh Museum", 
+      "Known for its medieval Astronomical Clock "]
+  ];
   let chosenTopic = topics[Math.floor(Math.random() * topics.length)];
   let word = chosenTopic[Math.floor(Math.random() * chosenTopic.length)];
   // word = word.replace(/\s/g, "-");
     console.log(word);
 
   if (chosenTopic === topics[0]) {
-    topicName.innerText = "WORD TOPIC: Historical Figures";
-    console.log("WORD TOPIC: Historical Figures")
+    wordTopic.innerText = "TOPIC: Historical Figures"; // <p id="wordTopic"></p>
+    console.log("Historical Figures")
   } else if (chosenTopic === topics[1]) {
-    topicName.innerText = "WORD TOPIC: Films";
-    console.log("WORD TOPIC: Films")
+    wordTopic.innerText = "TOPIC: Films";
+    console.log("Films")
   } else if (chosenTopic === topics[2]) {
-    topicName.innerText = "WORD TOPIC: Cities";
-    console.log("WORD TOPIC: Cities")
+    wordTopic.innerText = "TOPIC: Cities";
+    console.log("Cities")
   }
 }
 topicSelector();
