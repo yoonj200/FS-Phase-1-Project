104c0ae53ff77628af420d2e4e8828a5d3710f6a
Joshua <yoonj200@gmail.com>
2022-03-16 20:52:15 -0500

    selectWord() randomly selects a word and its respective hint

--- index.js
+++ index.js
@@ -14,49 +14,40 @@ function generateLetters() {
 }
 generateLetters();
 
-// Selects word
-function topicSelector() {
-  const topics = [
-    ["cleopatra", "napoleon-bonaparte", "julius-caesar", "marcus-aurelius", "abraham-lincoln", "grace-kelly", "akihito"],
-    ["alien", "dirty-harry", "the-matrix", "finding-nemo", "forrest-gump"],
-    ["taipei", "milan", "madrid", "amsterdam", "prague"] 
-  ];
-  hints = [
-    ["Last true pharaoh of Egypt", "French Emperor from 1804 to 1814", "Led Rome\'s armies in the Gallic Wars", 
-      "A stoic philosopher and Roman emperor", "16th President of the United States of America", 
-      "An early 20th-century American actress who became the Princess of Monaco", "Japan's first emperor to abdicate the throne since 1817"],
-    ["1979 Sci-fi horror film", "\"Go ahead. Make my day.\"", "\"Ignorance is bliss.\"", "\"Fish are friends, not food.\"", 
-      "\"I'm pretty tired... I think I'll go home now.\""],
-    ["Known for its Shilin market", "The global capital of fashion and design", "Home of the Prado Museum", "Home of the Van Gogh Museum", 
-      "Known for its medieval Astronomical Clock "]
-  ];
-  let chosenTopic = topics[Math.floor(Math.random() * topics.length)];
-  let word = chosenTopic[Math.floor(Math.random() * chosenTopic.length)];
-  // word = word.replace(/\s/g, "-");
-    console.log(word);
-
-  if (chosenTopic === topics[0]) {
-    wordTopic.innerText = "TOPIC: Historical Figures"; // <p id="wordTopic"></p>
-    console.log("Historical Figures")
-  } else if (chosenTopic === topics[1]) {
-    wordTopic.innerText = "TOPIC: Films";
-    console.log("Films")
-  } else if (chosenTopic === topics[2]) {
-    wordTopic.innerText = "TOPIC: Cities";
-    console.log("Cities")
+// Globally declared words and hints... is there a way around this?
+const wordHintPairs = [
+  {
+    "cleopatra": "last true pharaoh of Egypt",
+    "napolean-bonaparte": "French Emperor from 1804 to 1814",
+    "marcus-aurelius": "A stoic philosopher and Roman emperor",
+    "abraham-lincoln": "16th President of the United States of America",
+    "grace-kelly": "An early 20th-century American actress who became the Princess of Monaco"
+  },
+  {
+    "alien": "1979 Sci-fi horror film",
+    "dirty-harry": "\"Go ahead. Make my day.\"",
+    "the-matrix": "\"Ignorance is bliss.\"",
+    "finding-nemo": "\"Fish are friends, not food.\"",
+    "forrest-gump": "\"I'm pretty tired... I think I'll go home now.\""
+  },
+  {
+    "taipei": "Known for its Shilin market",
+    "milan": "The global capital of fashion and design",
+    "madrid": "Home of the Prado Museum",
+    "amsterdam": "Home of the Van Gogh Museum",
+    "prague": "Known for its medieval Astronomical Clock"
   }
+]
+// Selects word & hint
+function selectWord() {
+  // 'chosenObject' randomly selects nested object
+  let chosenObject = wordHintPairs[Math.floor(Math.random() * wordHintPairs.length)]
+  // console.log(chosenObject)
+  // 'pair' = randomly selected key-value pair from nested object
+  let pairs = Object.entries(chosenObject)
+  let pair = pairs[Math.floor(Math.random() * pairs.length)]
+  console.log(pair)
 }
-topicSelector();
-
-// Selects hint based off topicSelector
-
-
-/*
-function guessedWord() {
-  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
-
-  document.getElementById('wordSpotlight').innerHTML = wordStatus;
-}
-*/
+selectWord();
 
 })
