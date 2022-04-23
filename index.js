b01372708de8cceff9a21bcdf9548fd6859ea67b
Joshua <yoonj200@gmail.com>
2022-04-23 14:41:09 -0500

    Refactor for ES6 conformity (not finished)

--- index.js
+++ index.js
@@ -1,175 +1,155 @@
-window.addEventListener('DOMContentLoaded', () => {
-  // console.log('DOM fully loaded and parsed')
-// Globally stores game info ----------------------------------------
-  const gameObject = {
-    word:'', 
-    hint:'', 
-    storedGuesses: [],
-    myLives: 5
-  };
-// Initializes game state ----------------------------------------
-  function initGame() {
-    clue.textContent = "Need a Hint? Click the Hint button!";
-    blanks.textContent = "";
-    gameObject.storedGuesses = [];
-    gameObject.myLives = 5;
-    myLives.textContent = "You have " + gameObject.myLives + " attempts."
-    document.querySelectorAll(".keyboard .letter").forEach(element => element.disabled=false);
-    document.getElementById("hint").disabled=false;
-    selectWord();
-  }
-  initGame();
-// Generates responsive letter bank ----------------------------------------
-  let generateLetters = function() {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('') 
-    for (let i = 0; i < alphabet.length; i++) { // creates button for each substring
-      let btn = document.createElement('button');
-      let t = document.createTextNode(alphabet[i]);
+const gameObject = {
+  word:'',
+  hint:'',
+  storedGuesses: [],
+  myLives: 5
+};
 
-      btn.setAttribute('class', 'letter') // <button class="letter"> ... </button>
-      btn.appendChild(t);
-      document.querySelector(".keyboard").appendChild(btn); // appends to class="keyboard"
+const initGame = () => {
+  clue.textContent = 'Need a Hint? Click the Hint button!';
+  blanks.textContent = '';
+  gameObject.storedGuesses = [];
+  gameObject.myLives = 5;
+  myLives.textContent = 'You have ' + gameObject.myLives + ' attempts.'
+  document.querySelectorAll('.keyboard .letter').forEach(element => element.disabled=false);
+  document.getElementById('hint').disabled=false;
+  selectWord();
+}
 
-      btn.addEventListener("click", function() {
-        let evt = new CustomEvent("guess", {detail: {letter: alphabet[i]}, bubbles: true} )
-        this.dispatchEvent(evt)
-        this.disabled=true;
-      })
-    }
-    document.querySelector(".gameBoard").addEventListener("guess", handleGuesses, false)
-  }
-  generateLetters();
-// Randomly selects word & hint ----------------------------------------
-  function selectWord() {
-    fetch("http://localhost:3000/wordHintPair")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      let chosenObject = data[Math.floor(Math.random() * data.length)]
-      let chosenPairs = chosenObject.pairs
-      let chosenPairsIntoArray = Object.entries(chosenPairs) 
-      let chosenPair = chosenPairsIntoArray[Math.floor(Math.random() * chosenPairsIntoArray.length)]
-      // console.log(chosenObject)
-      // console.log(chosenPairs)
-      // console.log(chosenPairsIntoArray)
-      console.log(chosenPair)
-      
-      gameObject.word = chosenPair[0]
-      gameObject.hint = chosenPair[1]
-
-      if (chosenObject === data[0]) {
-        wordTopic.textContent = "TOPIC: " + chosenObject.topic;
-        console.log(chosenObject.topic)
-      } else if (chosenObject === data[1]) {
-        wordTopic.textContent = "TOPIC: " + chosenObject.topic;
-        console.log(chosenObject.topic)
-      } else if (chosenObject === data[2]) {
-        wordTopic.textContent = "TOPIC: " + chosenObject.topic;
-        console.log(chosenObject.topic)
-      } 
-      generateResults(gameObject.word);
-    })
-  }
-// Hint button ----------------------------------------
-  hint.addEventListener("click", function() {
-    clue.textContent = "HINT: " + gameObject.hint;
-    document.getElementById("hint").disabled = true; // Hint button can only be clicked once
+const selectWord = () => {
+  fetch('http://localhost:3000/wordHintPair')
+  .then(function (response) {
+    return response.json();
   })
-// Play again button ----------------------------------------
-  reset.addEventListener("click", function() {
+  .then(function (data) {
+    let chosenObject = data[Math.floor(Math.random() * data.length)]
+    let chosenPairs = chosenObject.pairs
+    let chosenPairsIntoArray = Object.entries(chosenPairs) 
+    let chosenPair = chosenPairsIntoArray[Math.floor(Math.random() * chosenPairsIntoArray.length)]
+    console.log(chosenPair)
+    
+    gameObject.word = chosenPair[0]
+    gameObject.hint = chosenPair[1]
+
+    wordTopic.textContent = 'TOPIC: ' + chosenObject.topic;
+    console.log(chosenObject.topic)
+
+    generateBlanks(gameObject.word.split(''));
+  })
+}
+
+const generateBlanks = (letters) => {
+  let wordHolder = document.getElementById('blanks');
+  let correct = document.createElement('ul');
+  correct.setAttribute('id', 'my-word');
+
+  letters.forEach(letter => {
+    let letterHolder = document.createElement('li');
+    letterHolder.setAttribute('class', 'guess');
+    if (letter === '-') {
+      letterHolder.textContent = ' ';
+    } else {
+      letterHolder.textContent = '_';
+    }
+    correct.appendChild(letterHolder);
+    wordHolder.appendChild(correct);
+  })
+}
+
+const generateLetters = () => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('') 
+  alphabet.forEach(letter => { 
+    let btn = document.createElement('button');
+    let t = document.createTextNode(letter);
+
+    btn.setAttribute('class', 'letter')
+    btn.appendChild(t);
+    document.querySelector('.keyboard').appendChild(btn);
+
+    btn.addEventListener('click', function() {
+      let evt = new CustomEvent('guess', {detail: {letter: letter}, bubbles: true} )
+      this.dispatchEvent(evt);
+      this.disabled=true;
+    })
+  })
+  document.querySelector('.gameBoard').addEventListener('guess', handleGuesses, false)
+}
+
+const handleGuesses = ({detail}) => {
+  gameObject.storedGuesses.push(detail.letter); 
+  console.log(gameObject.storedGuesses)
+  
+  if (gameObject.word.includes(detail.letter)) {
+    let letterPositions = findAllLetterPositions(gameObject.word, detail.letter) 
+    letterPositions.forEach(position => document.querySelectorAll('li.guess')[position].textContent = detail.letter)
+    gameSuccess()
+  } else { 
+      gameObject.myLives -=1 
+      myLives.textContent = 'You have ' + gameObject.myLives + ' remaining attempts.'
+      gameOver();
+    }
+}
+
+const gameSuccess = () => {
+  let guessListElements = document.querySelectorAll('li.guess')
+      let checkBlanks = Array.from(guessListElements).filter(element => element.textContent === "_").length
+      if (checkBlanks === 0) {
+        fetchFact()
+        gameOver()
+        document.querySelectorAll('.keyboard .letter').forEach(element => element.disabled=true);
+        document.getElementById('hint').disabled=true;
+      }
+}
+
+const gameOver = () => {
+  if (gameObject.myLives < 1) {
+    myLives.textContent = 'GAME OVER! Try again!'
+    document.querySelectorAll('.keyboard .letter').forEach(element => element.disabled=true);
+    document.getElementById('hint').disabled=true;
+  }
+}
+
+const findAllLetterPositions = (haystack, needle) => {
+  let position = 0;
+  let positions = [];
+  while(haystack.indexOf(needle, position) > -1) {
+    position = haystack.indexOf(needle, position) + 1;
+    positions.push(position - 1)
+  }
+  return positions;
+}
+
+const fetchFact = () => {
+  fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+  .then(response => response.json())
+  .then( data => {
+    myLives.textContent = `You win! Here's your random fact: ` + data[`text`];
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  initGame();
+  generateLetters();
+
+  hint.addEventListener('click', () => {
+    clue.textContent = 'HINT: ' + gameObject.hint;
+    document.getElementById('hint').disabled = true;
+  })
+  reset.addEventListener('click', () => {
     initGame();
   })
-// Generates blanks for chosen word ----------------------------------------
-  function generateResults(letters) {
-    let wordHolder = document.getElementById('blanks');
-    let correct = document.createElement('ul');
-    correct.setAttribute('id', 'my-word');
+  
+  // const themeButton = document.createElement('button');
+  // themeButton.setAttribute('id', 'themeButton');
+  // themeButton.innerText = 'Theme';
+  // document.body.appendChild(themeButton);
 
-    for (let i = 0; i < letters.length; i++) {
-      let letterHolder = document.createElement('li');
-      letterHolder.setAttribute('class', 'guess');
-      if (letters[i] === "-") {
-        letterHolder.textContent = " ";
-      } else {
-        letterHolder.textContent = "_";
-      }
-      correct.appendChild(letterHolder);
-      wordHolder.appendChild(correct);
-    }
-  }
+  let expandingList = document.createElement('ul', { is : 'expanding-list' })
+  document.createElement('button', { 'id' : 'themeButton', 'innerText' : 'Theme' })
 
-// Handles user's guesses ---------------------------------------- 
-  function handleGuesses(guess) {
-    // CustomEvent.detail returns any data passed when initializing event
-    gameObject.storedGuesses.push(guess.detail.letter); 
-    console.log(gameObject.storedGuesses)
-    // if chosen word contains guessed letter, blank is replaced with guessed letter
-    if (gameObject.word.includes(guess.detail.letter)) {
-      let letterPositions = findAllLetterPositions(gameObject.word, guess.detail.letter) 
-      letterPositions.forEach(position => document.querySelectorAll("li.guess")[position].textContent = guess.detail.letter)
-      gameSuccess()
-    } else { 
-        gameObject.myLives -=1 // incorrect guesses deduct a life
-        myLives.textContent = "You have " + gameObject.myLives + " remaining attempts."
-        gameOver();
-      }
-  }
-// Game success ----------------------------------------
-  function gameSuccess() {
-    let guessListElements = document.querySelectorAll("li.guess")
-        let checkBlanks = Array.from(guessListElements).filter(element => element.textContent === "_").length
-        if (checkBlanks === 0) {
-          fetchFact()
-          gameOver()
-          document.querySelectorAll(".keyboard .letter").forEach(element => element.disabled=true);
-          document.getElementById("hint").disabled=true;
-        }
-  }
-// Game Over ----------------------------------------
-  function gameOver() {
-    if (gameObject.myLives < 1) {
-      myLives.textContent = "GAME OVER! Try again!"
-      document.querySelectorAll(".keyboard .letter").forEach(element => element.disabled=true);
-      document.getElementById("hint").disabled=true;
-    }
-  }
-// Finds and stores letter positions ----------------------------------------
-  function findAllLetterPositions(haystack, needle) {
-    let position = 0;
-    let positions = [];
-    while(haystack.indexOf(needle, position) > -1) {
-      position = haystack.indexOf(needle, position) + 1;
-      positions.push(position - 1)
-    }
-    return positions;
-  }
-// Fetches API data (random fact) ----------------------------------------
-  function fetchFact() {
-  // Call 'fetch()' and pass the URL to a data source as the argument. 
-  // 'fetch()' returns a Promise object representing datasource sends back
-    fetch("https://uselessfacts.jsph.pl/random.json?language=en")
-    // Next, call `then()` on the Promise object returned by calling
-    // `fetch()`. `then()` takes a callback function as an argument
-    .then(function (response) {
-      // Callback function processes object
-      return response.json();
-      // parses response (JSON-formatted **string**) into JS **object**
-    })
-    // The next 'then()' receives parsed JSON object returned form first 'then()'
-    .then(function (data) {
-      // Second callback function performs DOM manipulation using data returned from server
-      myLives.textContent = `You win! Here's your random fact: ` + data[`text`];
-    });
-  }
-// Theme button ----------------------------------------
-  let themeButton = document.createElement('button');
-  themeButton.setAttribute('id', 'themeButton');
-  themeButton.innerText = "Theme";
-  document.body.appendChild(themeButton);
-
-  themeButton.addEventListener('click', function() {
+  themeButton.addEventListener('click', () => {
     document.body.classList.toggle('theme');
   })
 
+   
 })
